de0924e7d3f3e5e4e2b32cf8641f86421e34ee8d
Petr Albrecht <albrecht@citicash.io>
2018-03-14 11:29:03 +0100

    Solution of problem with diff calculation moved from V2 into V1 - Discussion about the problem here   https://github.com/monero-project/research-lab/issues/3

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -47,12 +47,9 @@
 #define MIN_TRANSACTION_VERSION							            2
 #define CURRENT_BLOCK_MAJOR_VERSION                     1
 #define CURRENT_BLOCK_MINOR_VERSION                     1
-#define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT              60*60*2
+#define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT              60*24
 #define CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE             10
-#define BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW               60
-
-#define CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT_V2           60*24
-#define BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW_V2            12
+#define BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW               12
 
 // MONEY_SUPPLY - total number coins to be generated
 #define MONEY_SUPPLY                                    ((uint64_t)1000000000000000000)
@@ -74,14 +71,10 @@
 #define ORPHANED_BLOCKS_MAX_COUNT                       100
 
 #define DIFFICULTY_TARGET                               120  // seconds
-#define DIFFICULTY_WINDOW                               125  // blocks
+#define DIFFICULTY_WINDOW                               17  // blocks
 #define DIFFICULTY_LAG                                  15   // !!!
-#define DIFFICULTY_CUT                                  60   // timestamps to cut after sorting
-#define DIFFICULTY_BLOCKS_COUNT                         DIFFICULTY_WINDOW + DIFFICULTY_LAG
-
-#define DIFFICULTY_WINDOW_V2							              17
-#define DIFFICULTY_CUT_V2                               6
-#define DIFFICULTY_BLOCKS_COUNT_V2                      DIFFICULTY_WINDOW_V2 + DIFFICULTY_CUT_V2*2
+#define DIFFICULTY_CUT                                  6   // timestamps to cut after sorting
+#define DIFFICULTY_BLOCKS_COUNT                         DIFFICULTY_WINDOW + DIFFICULTY_CUT*2
 
 #define CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS       1
 #define CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS      DIFFICULTY_TARGET * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS
--- src/cryptonote_core/difficulty.cpp
+++ src/cryptonote_core/difficulty.cpp
@@ -124,54 +124,10 @@ namespace cryptonote {
 
   difficulty_type next_difficulty(std::vector<std::uint64_t> timestamps, std::vector<difficulty_type> cumulative_difficulties, size_t target_seconds) {
 
-    if (timestamps.size() > DIFFICULTY_WINDOW)
+    if (timestamps.size() > DIFFICULTY_BLOCKS_COUNT)
     {
-      timestamps.resize(DIFFICULTY_WINDOW);
-      cumulative_difficulties.resize(DIFFICULTY_WINDOW);
-    }
-
-
-    size_t length = timestamps.size();
-    assert(length == cumulative_difficulties.size());
-    if (length <= 1) {
-      return 1;
-    }
-    static_assert(DIFFICULTY_WINDOW >= 2, "Window is too small");
-    assert(length <= DIFFICULTY_WINDOW);
-    sort(timestamps.begin(), timestamps.end());
-    size_t cut_begin, cut_end;
-    static_assert(2 * DIFFICULTY_CUT <= DIFFICULTY_WINDOW - 2, "Cut length is too large");
-    if (length <= DIFFICULTY_WINDOW - 2 * DIFFICULTY_CUT) {
-      cut_begin = 0;
-      cut_end = length;
-    }
-    else {
-      cut_begin = (length - (DIFFICULTY_WINDOW - 2 * DIFFICULTY_CUT) + 1) / 2;
-      cut_end = cut_begin + (DIFFICULTY_WINDOW - 2 * DIFFICULTY_CUT);
-    }
-    assert(/*cut_begin >= 0 &&*/ cut_begin + 2 <= cut_end && cut_end <= length);
-    uint64_t time_span = timestamps[cut_end - 1] - timestamps[cut_begin];
-    if (time_span == 0) {
-      time_span = 1;
-    }
-    difficulty_type total_work = cumulative_difficulties[cut_end - 1] - cumulative_difficulties[cut_begin];
-    assert(total_work > 0);
-    uint64_t low, high;
-    mul(total_work, target_seconds, low, high);
-    // blockchain errors "difficulty overhead" if this function returns zero.
-    // TODO: consider throwing an exception instead
-    if (high != 0 || low + time_span - 1 < low) {
-      return 0;
-    }
-    return (low + time_span - 1) / time_span;
-  }
-
-  difficulty_type next_difficulty_v2(std::vector<std::uint64_t> timestamps, std::vector<difficulty_type> cumulative_difficulties, size_t target_seconds) {
-
-    if (timestamps.size() > DIFFICULTY_BLOCKS_COUNT_V2)
-    {
-      timestamps.resize(DIFFICULTY_BLOCKS_COUNT_V2);
-      cumulative_difficulties.resize(DIFFICULTY_BLOCKS_COUNT_V2);
+      timestamps.resize(DIFFICULTY_BLOCKS_COUNT);
+      cumulative_difficulties.resize(DIFFICULTY_BLOCKS_COUNT);
     }
 
     size_t length = timestamps.size();
@@ -182,14 +138,14 @@ namespace cryptonote {
 
     sort(timestamps.begin(), timestamps.end());
     size_t cut_begin, cut_end;
-    static_assert(2 * DIFFICULTY_CUT_V2 <= DIFFICULTY_BLOCKS_COUNT_V2 - 2, "Cut length is too large");
-    if (length <= DIFFICULTY_BLOCKS_COUNT_V2 - 2 * DIFFICULTY_CUT_V2) {
+    static_assert(2 * DIFFICULTY_CUT <= DIFFICULTY_BLOCKS_COUNT - 2, "Cut length is too large");
+    if (length <= DIFFICULTY_BLOCKS_COUNT - 2 * DIFFICULTY_CUT) {
       cut_begin = 0;
       cut_end = length;
     }
     else {
-      cut_begin = (length - (DIFFICULTY_BLOCKS_COUNT_V2 - 2 * DIFFICULTY_CUT_V2) + 1) / 2;
-      cut_end = cut_begin + (DIFFICULTY_BLOCKS_COUNT_V2 - 2 * DIFFICULTY_CUT_V2);
+      cut_begin = (length - (DIFFICULTY_BLOCKS_COUNT - 2 * DIFFICULTY_CUT) + 1) / 2;
+      cut_end = cut_begin + (DIFFICULTY_BLOCKS_COUNT - 2 * DIFFICULTY_CUT);
     }
     assert(/*cut_begin >= 0 &&*/ cut_begin + 2 <= cut_end && cut_end <= length);
     uint64_t total_timespan = timestamps[cut_end - 1] - timestamps[cut_begin];
