bb9b3d7af3ff8ee94a6a3cf56d8f4b59bdb0dd9d
Lukas Salich <salich@citicash.io>
2018-04-16 15:38:10 +0200

    workaround for sending all data for alias

--- src/cryptonote_core/tx_pool.h
+++ src/cryptonote_core/tx_pool.h
@@ -523,6 +523,3 @@ namespace boost
 }
 BOOST_CLASS_VERSION(cryptonote::tx_memory_pool, CURRENT_MEMPOOL_ARCHIVE_VER)
 BOOST_CLASS_VERSION(cryptonote::tx_memory_pool::tx_details, CURRENT_MEMPOOL_TX_DETAILS_ARCHIVE_VER)
-
-
-
