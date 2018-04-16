0d5981d3fa6095666fb923abafd5664a47fb1331
Jakub Silar <silar@citicash.io>
2018-04-16 13:53:57 +0200

    New Genesis block for cryptonight

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -162,7 +162,7 @@ namespace config
   boost::uuids::uuid const NETWORK_ID = { {
       0xdf, 0xce, 0xfc, 0x7c, 0x27, 0x4a, 0x24, 0xd4, 0xf3, 0x8d, 0x42, 0x44, 0x60, 0xa8, 0x04, 0x06
     } }; // Bender's nightmare
-  std::string const GENESIS_TX = "020a01ff00018080a8ec85afd1b101029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101398eb8bb0af23ea7e29c2cd4813dc534a0fe87a8715d19c1bee0384408b40a1200";
+  std::string const GENESIS_TX = "020a01ff00018080b6a58eb9e7ef01029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101bc62411264530893dafe4255858654eb33ba2cb1fb7322ba07ff364b77931b5d00";
   uint32_t const GENESIS_NONCE = 10000;
 
   namespace testnet
@@ -176,7 +176,7 @@ namespace config
     boost::uuids::uuid const NETWORK_ID = { {
         0x06, 0xdf, 0xce, 0xfc, 0x7c, 0x27, 0x4a, 0x24, 0xd4, 0xf3, 0x8d, 0x42, 0x44, 0x60, 0x12, 0x04
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "020a01ff00018080a8ec85afd1b101029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101e4c250c8d7b19bfcd0951012415b03f6579ab77fca72c2393efd0f1a0a2ead0200";
+    std::string const GENESIS_TX = "020a01ff00018080b6a58eb9e7ef01029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101aa257f838d9b7fff5df3bc1871e7118886593fa90c1ab4e2de191f1a6ec5889000";
     uint32_t const GENESIS_NONCE = 10001;
   }
 }
