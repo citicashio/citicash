6789922544d5afb0c11b4c74fc9310a2b34f3a77
Lukas Salich <salich@citicash.io>
2018-07-16 13:03:55 +0200

    added ' ' as legal character for payment_id

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -2375,7 +2375,7 @@ bool wallet2::parse_payment_note(const std::string &payment_id_str, std::string 
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
             'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '/', '-', '?', ':', '(', ')', '.', ',', '_', '+'
+            '/', '-', '?', ':', '(', ')', '.', ',', '_', '+', ' '
     };
     unsigned int countlegal = std::count_if(payment_id_str.begin(), payment_id_str.end(),
                                             [&pay_id_charset](char c) { return pay_id_charset.count(c); });
